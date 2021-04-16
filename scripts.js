926893e04f8ff1560ca7b8dcfb12088bd0ae408f
nicolaslaje <nicolaje35@gmail.com>
2021-04-16 01:45:29 -0300

    Master #3 Working on Slider.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+output.innerHTML = slider.value;
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
+slider.oninput = function() {
   output.innerHTML = this.value;
-};
